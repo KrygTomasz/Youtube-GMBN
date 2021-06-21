fd7219b4dee46c48a9f0e53e1eebd2f201d070da
Kryg Tomasz <tomasz.kryg@gmail.com>
2021-06-21 17:58:13 +0200

    Prepare empty list view and viewModel

---
+++ YoutubeGMBN/YoutubeGMBN/Sources/Scenes/List/View/MoviesListViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MoviesListViewController.swift
+//  YoutubeGMBN
+//
+//  Created by Tomasz Kryg on 21/06/2021.
+//
+
+import UIKit
+import GMBNNetworking
+
+final class MoviesListViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let viewModel: MoviesListViewModel
+    
+    init(viewModel: MoviesListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(tableView)
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
---
+++ YoutubeGMBN/YoutubeGMBN/Sources/Scenes/List/ViewModel/MoviesListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesListViewModel.swift
+//  YoutubeGMBN
+//
+//  Created by Tomasz Kryg on 21/06/2021.
+//
+
+import Foundation
+
+final class MoviesListViewModel {
+    
+}
