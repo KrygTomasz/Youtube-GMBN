654e6dc5b34b50fceacbea4ac8580462cf650921
Tomasz Kryg <tomasz.kryg@siroccomobile.com>
2021-06-21 17:03:36 +0200

    Initial commit

---
+++ YoutubeGMBN/YoutubeGMBN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeGMBN
+//
+//  Created by Tomasz Kryg on 21/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
